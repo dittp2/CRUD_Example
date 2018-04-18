4f5433ea88325c95ea67953b497aeed15e127764
dittp2 <pascal.dittli@outlook.com>
2018-04-18 22:31:25 +0200

    sd

---
+++ HealthProfessionalPortal/src/HealthProfessionalPortal/src/main/java/com/example/controllers/UserController.java
@@ -0,0 +1,52 @@
+package com.example.controllers;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entities.User;
+import com.example.repositories.UserRepository;
+
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+	@Autowired
+	private UserRepository userRepository;
+	
+	@GetMapping("/users")
+	public List<User> getUsers() {
+		return userRepository.findAll();
+	}
+	
+	@GetMapping("/users/{id}")
+	public User getUsers(@PathVariable Long id) {
+		return userRepository.findOne(id);
+	}
+	
+	
+	@DeleteMapping("/users/{id}")
+	public boolean deleteUser(@PathVariable Long id) {
+		userRepository.delete(id);
+		return true;
+	}
+
+	@PostMapping("/user")
+	public User createUser(User user) {
+		return userRepository.save(user);
+	}
+	
+	@PutMapping("/user")
+	public User updateUser(User user) {
+		return userRepository.save(user);
+	}	
+
+}
